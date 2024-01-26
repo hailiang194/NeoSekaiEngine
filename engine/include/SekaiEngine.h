7c6b7a6ff3080e00b86c4b6689c737a326e827db
Luong The Hai <hailuongthe2000@gmail.com>
2024-01-26 13:31:32 +0700

    Add Render params for shapes

--- engine/include/SekaiEngine.h
+++ engine/include/SekaiEngine.h
@@ -14,5 +14,6 @@
 #include "SekaiEngine/Render/Params/TextureRenderParams.h"
 #include "SekaiEngine/Render/RendererAPI.h"
 #include "SekaiEngine/Input.h"
+#include "SekaiEngine/Render/Params/RectangleRenderParams.h"
 
 #endif //!_SEKAI_ENGINE_SEKAI_ENGINE_H_
--- sandbox/src/Sandbox.cpp
+++ sandbox/src/Sandbox.cpp
@@ -5,13 +5,13 @@ class ExampleLayer: public SekaiEngine::Layer::Layer
 {
 public:
     ExampleLayer()
-        :Layer("Hello friend"), camera(), texture("icon.png"), position()
+        :Layer("Hello friend"), camera(), rect(SekaiEngine::Math::Vector2D(), 100, 50)
     {
 
     }
 
     ExampleLayer(const ExampleLayer& layer)
-        :Layer(layer), camera(layer.camera), texture(layer.texture), position(layer.position)
+        :Layer(layer), camera(layer.camera), rect(layer.rect)
     {
 
     }
@@ -24,14 +24,14 @@ public:
     void OnUpdate(const SekaiEngine::Timestep& elipse) override
     {
         if(SekaiEngine::Input::IsKeyPressed(SekaiEngine::Input::KEY_A))
-            position.X() -= (100 * elipse.ToSeconds());
+            rect.Position.X() -= (100 * elipse.ToSeconds());
         else if(SekaiEngine::Input::IsKeyPressed(SekaiEngine::Input::KEY_D))
-            position.X() += (100 * elipse.ToSeconds());
+            rect.Position.X() += (100 * elipse.ToSeconds());
 
         if(SekaiEngine::Input::IsKeyPressed(SekaiEngine::Input::KEY_W))
-            position.Y() -= (100 * elipse.ToSeconds());
+            rect.Position.Y() -= (100 * elipse.ToSeconds());
         else if(SekaiEngine::Input::IsKeyPressed(SekaiEngine::Input::KEY_S))
-            position.Y() += (100 * elipse.ToSeconds());
+            rect.Position.Y() += (100 * elipse.ToSeconds());
 
         SekaiEngine::Render::RenderCommand::SetClearColor((SekaiEngine::Render::Color)0xffffffff);
         SekaiEngine::Render::RenderCommand::Clear();
@@ -39,15 +39,15 @@ public:
         SekaiEngine::Render::Renderer::BeginScreen(camera);
 
         SekaiEngine::Render::RenderProperties props;
-        SekaiEngine::Render::TextureRenderParams params(props, texture, position);
-        SekaiEngine::Render::API::DrawTexture(params.Texture(), params.Tint(), params.Source(), params.Destination(), params.Origin(), params.Rotation());
+        props.Tint = 0xff0000ff;
+        SekaiEngine::Render::RectangleRenderParams params(props, rect);
+        SekaiEngine::Render::API::DrawRect(params.DrawRect(), params.Tint(), params.Origin(), params.Rotation());
 
         SekaiEngine::Render::Renderer::EndScreen();
     }
 private:
     SekaiEngine::Render::Camera2D camera;
-    SekaiEngine::Render::Texture texture;
-    SekaiEngine::Math::Vector2D position;
+    SekaiEngine::Shape::Rectangle rect;
 };
 
 
