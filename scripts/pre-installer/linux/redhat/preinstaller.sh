c68d9e17b123e07ed8f13929072a1c08746a25d8
Luong The Hai <hailuongthe2000@gmail.com>
2024-03-30 23:24:15 +0700

    Add libatomic for redhat

--- scripts/pre-installer/linux/redhat/preinstaller.sh
+++ scripts/pre-installer/linux/redhat/preinstaller.sh
@@ -3,4 +3,4 @@ dnf -v &> /dev/null
 if [ $? -ne 0 ]; then
         yum install -y dnf &> /dev/null
 fi
-sudo dnf -y install git cmake make gcc-c++ alsa-lib-devel mesa-libGL-devel libX11-devel libXrandr-devel libXi-devel libXcursor-devel libXinerama-devel
+sudo dnf -y install git cmake make gcc-c++ alsa-lib-devel mesa-libGL-devel libX11-devel libXrandr-devel libXi-devel libXcursor-devel libXinerama-devel libatomic
