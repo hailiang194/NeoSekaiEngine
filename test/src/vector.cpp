d1b77405b4fe6e89b0dae832ec5f5608de8bbbf7
Luong The Hai <hailuongthe2000@gmail.com>
2024-03-30 16:24:58 +0700

    #6 - Add test

--- test/src/vector.cpp
+++ test/src/vector.cpp
@@ -84,3 +84,55 @@ TEST(EngineTest, TestVector2D_Dot)
     SekaiEngine::Math::Vector2D test(3.0f, 4.0f);
     EXPECT_TRUE(SekaiEngine::Math::cmpFloat(test.Dot(test), 25.0f) == 0);
 }
+
+TEST(EngineTest, TestVector2D_AssignAdd)
+{
+    SekaiEngine::Math::Vector2D op1(3.0f, 4.0f);
+    SekaiEngine::Math::Vector2D op2(3.0f, 4.0f);
+
+    SekaiEngine::Math::Vector2D result = op1 + op2;
+    EXPECT_TRUE(result == SekaiEngine::Math::Vector2D(6.0f, 8.0f));
+}
+
+TEST(EngineTest, TestVector2D_AssignMinus)
+{
+    SekaiEngine::Math::Vector2D op1(3.0f, 4.0f);
+    SekaiEngine::Math::Vector2D op2(3.0f, 4.0f);
+
+    SekaiEngine::Math::Vector2D result = op1 - op2;
+    EXPECT_TRUE(result == SekaiEngine::Math::Vector2D(0.0f, 0.0f));
+}
+
+TEST(EngineTest, TestVector2D_AssignScale)
+{
+    SekaiEngine::Math::Vector2D op1(3.0f, 4.0f);
+
+    SekaiEngine::Math::Vector2D result = op1 * 0.5f;
+    EXPECT_TRUE(result == SekaiEngine::Math::Vector2D(1.5f, 2.0f));
+}
+
+TEST(EngineTest, TestVector3D_AssignAdd)
+{
+    SekaiEngine::Math::Vector3D op1(3.0f, 4.0f, 5.0f);
+    SekaiEngine::Math::Vector3D op2(3.0f, 4.0f, 5.0f);
+
+    SekaiEngine::Math::Vector3D result = op1 + op2;
+    EXPECT_TRUE(result == SekaiEngine::Math::Vector3D(6.0f, 8.0f, 10.0f));
+}
+
+TEST(EngineTest, TestVector3D_AssignMinus)
+{
+    SekaiEngine::Math::Vector3D op1(3.0f, 4.0f, 5.0f);
+    SekaiEngine::Math::Vector3D op2(3.0f, 4.0f, 5.0f);
+
+    SekaiEngine::Math::Vector3D result = op1 - op2;
+    EXPECT_TRUE(result == SekaiEngine::Math::Vector3D(0.0f, 0.0f, 0.0f));
+}
+
+TEST(EngineTest, TestVector3D_AssignScale)
+{
+    SekaiEngine::Math::Vector3D op1(3.0f, 4.0f, 5.0f);
+
+    SekaiEngine::Math::Vector3D result = op1 * 0.5f;
+    EXPECT_TRUE(result == SekaiEngine::Math::Vector3D(1.5f, 2.0f, 2.5f));
+}
