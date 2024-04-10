8938010b3907c1614fe292542fe53f5aa0a8ab06
Luong The Hai <hailuongthe2000@gmail.com>
2024-04-10 21:45:05 +0700

    Fix attach and dettach for layer

--- engine/src/SekaiEngine/Layer/LayerStack.cpp
+++ engine/src/SekaiEngine/Layer/LayerStack.cpp
@@ -36,11 +36,13 @@ namespace SekaiEngine
         void LayerStack::PushLayer(Layer* layer)
         {
             m_layerInsert = m_layers.emplace(m_layerInsert, layer);
+            layer->OnAttach();
         }
 
         void LayerStack::PushOverlay(Layer* overlay)
         {
             m_layers.emplace_back(overlay);
+            overlay->OnAttach();
         }
 
         void LayerStack::PopLayer(Layer* layer)
@@ -50,6 +52,7 @@ namespace SekaiEngine
             {
                 m_layers.erase(it);
                 --m_layerInsert;
+                layer->OnDetach();
             }
         }
 
@@ -59,6 +62,7 @@ namespace SekaiEngine
             if(it != m_layers.end())
             {
                 m_layers.erase(it);
+                overlay->OnDetach();
             }
         }
     } // namespace Layer
