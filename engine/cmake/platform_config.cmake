872013819bd3277ea7b879dd2678a1a17fbef0a2
Luong The Hai <hailuongthe2000@gmail.com>
2024-08-26 22:50:41 +0700

    Add asset_path for platform_config

--- engine/cmake/platform_config.cmake
+++ engine/cmake/platform_config.cmake
@@ -1,7 +1,7 @@
 # PlatformConfig.cmake
 
 # Define a function to configure platform-specific and Web-specific settings
-function(configure_platform_and_web target)
+function(configure_platform_and_web target asset_path)
     # Apple-specific configuration
     if(APPLE)
         target_link_libraries(${target} PRIVATE "-framework IOKit"
@@ -13,14 +13,15 @@ function(configure_platform_and_web target)
         set_target_properties(${target} PROPERTIES SUFFIX ".html")
 
         if(EMSCRIPTEN)
-            set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -s USE_GLFW=3 -s ASSERTIONS=1 -s WASM=1 -s ASYNCIFY --preload-file ${PROJECT_SOURCE_DIR}/assets@" PARENT_SCOPE)
-            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -s USE_GLFW=3 -s ASSERTIONS=1 -s WASM=1 -s ASYNCIFY  --preload-file ${PROJECT_SOURCE_DIR}/assets@" PARENT_SCOPE)
+            set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -s USE_GLFW=3 -s ASSERTIONS=1 -s WASM=1 -s ASYNCIFY --preload-file ${asset_path}@" PARENT_SCOPE)
+            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -s USE_GLFW=3 -s ASSERTIONS=1 -s WASM=1 -s ASYNCIFY  --preload-file ${asset_path}@" PARENT_SCOPE)
             set(CMAKE_EXECUTABLE_SUFFIX ".html" PARENT_SCOPE)
-        else()
-            add_custom_command(TARGET ${target} POST_BUILD
-                COMMAND ${CMAKE_COMMAND} -E copy_directory
-                    "${PROJECT_SOURCE_DIR}/assets"
-                    $<TARGET_FILE_DIR:${target}>)
         endif()
+    else()
+        add_custom_command(TARGET ${target} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy_directory
+            "${asset_path}"
+            $<TARGET_FILE_DIR:${target}>)
     endif()
+    
 endfunction()
